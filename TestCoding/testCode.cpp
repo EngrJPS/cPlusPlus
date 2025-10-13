28131a52abd36681e3de8db9fa2b5afcf33a985d
EngrJPS <paul.serafin@hcdc.edu.ph>
2025-10-13 16:13:12 +0800

    Tried the Array properties and compare it to pointers

--- TestCoding/testCode.cpp
+++ TestCoding/testCode.cpp
@@ -3,5 +3,13 @@ using namespace std;
 
 int main(){
     cout << "Hello universe!" << endl;
+
+    int arrNum[] {100, 95, 80};
+
+
+    cout << arrNum  << endl;
+    cout << (arrNum + 1) << endl;
+    cout << (arrNum + 2) << endl;
+    
     return 0;
 }
