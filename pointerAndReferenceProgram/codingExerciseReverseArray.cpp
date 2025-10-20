3dc4927661f2e1314958745f926c108194ffbcee
EngrJPS <paul.serafin@hcdc.edu.ph>
2025-10-20 19:57:08 +0800

    Rewrote the reverse array challenge

--- pointerAndReferenceProgram/codingExerciseReverseArray.cpp
+++ pointerAndReferenceProgram/codingExerciseReverseArray.cpp
@@ -1,33 +1,76 @@
+/*
+Exercise: Array Reversal using Pointers
+
+Specification:
+
+Write a C++ function reverse_array that takes an integer array and its size as input and reverses the order of the elements in the array. 
+The function should modify the array in-place.
+
+Input:
+
+arr: A pointer to the first element of the integer array.
+
+size: The size of the array.
+*/
+
 #include<iostream>
 using namespace std;
 
-void reverse_array(int*, int);
+void revArray(int*, int);
 
 int main(){
 
-    int arr[] = {1, 2, 3, 4, 5};
+    int arr[] {1, 2, 3, 4, 5, 6};
     int size = sizeof(arr) / sizeof(arr[0]);
- 
-    reverse_array(arr, size);
-    
+    revArray(arr, size);
+
     cout << "{ ";
-    for(size_t i{0}; i < size; i++){
-        cout << arr[i] << " ";
+    for(auto num : arr){
+        cout << num << " ";
     }
-    cout << " }" << endl;
+
+    cout << "}";
+
     return 0;
 }
 
-void reverse_array(int *arr, int size){
+void revArray(int *array, int size){
+    int *first = array;
+    int *last = array + size - 1;
 
-    int *start = arr;
-    int *last = arr+size-1;
-
-    for(start; start < last; start++){
-        int temp = *start;
-        *start = *last;
+    for(first; first < last; first++){
+        int temp = *first;
+        *first = *last;
         *last = temp;
         last--;
     }
-    
 }
+
+
+/*
+    1: 1 < 6    temp: 1, last: temp
+    Before:
+    {1, 2, 3, 4, 5, 6}
+     |              |
+     f              l
+
+     After:
+    {6, 2, 3, 4, 5, 1}
+
+    2: 2 < 5    temp: 2, last: temp
+    Before:
+    {6, 2, 3, 4, 5, 1}
+        |        |
+        f        l
+    
+    After:
+    {6, 5, 3, 4, 2, 1}
+
+    3: 3 < 4    temp: 3, last: temp
+    Before:
+    {6, 5, 3, 4, 2, 1}
+           |  |
+           f  l
+    After:
+    {6, 5, 4, 3, 2, 1}
+*/
