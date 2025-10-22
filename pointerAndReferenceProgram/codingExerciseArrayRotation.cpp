15cb4211627e77d805a6d38a858ab28505b87049
EngrJPS <paul.serafin@hcdc.edu.ph>
2025-10-22 15:58:39 +0800

    Added sumOfDigits function to practice the recursion problem and Revised the Array Rotation program

--- pointerAndReferenceProgram/codingExerciseArrayRotation.cpp
+++ pointerAndReferenceProgram/codingExerciseArrayRotation.cpp
@@ -7,24 +7,31 @@ void rotate_array(int* arr, int size, int k) {
     int *last {arr + size - k};
     int *temp = new int[size];
 
-    int count{0};
+    if(size < k){
+        while(first < last){
+            int temp = *first;
+            *first = *last;
+            *last = temp;
+        }
+    }else{
 
-    for(int i{0}; i < k; i++){
-        temp[i] = *last;
-        last++;
-        count++; // 2
+        int count{0};
+
+        for(int i{0}; i < k; i++){
+            temp[i] = *last;
+            last++;
+            count++; // 2
+        }
+
+        for(int i{count}; i < size; i++){
+            temp[i] = *first;
+            first++;
+        }
+
+        for(int i{0}; i < size; i++){
+            arr[i] = temp[i];
+        }
     }
-
-    for(int i{count}; i < size; i++){
-        temp[i] = *first;
-        first++;
-    }
-
-    for(int i{0}; i < size; i++){
-        arr[i] = temp[i];
-    }
-
-
 }
 
 void print_array(int* arr, int size) {
@@ -40,10 +47,10 @@ int main() {
     rotate_array(arr1, size1, 2);
     print_array(arr1, size1);  // Expected: 4 5 1 2 3
     
-    // int arr2[] = {10, 20, 30};
-    // int size2 = 3;
-    // rotate_array(arr2, size2, 1);
-    // print_array(arr2, size2);  // Expected: 30 10 20
+    int arr2[] = {10, 20, 30};
+    int size2 = 3;
+    rotate_array(arr2, size2, 1);
+    print_array(arr2, size2);  // Expected: 30 10 20
     
     return 0;
 }
@@ -53,10 +60,22 @@ int main() {
     [1, 2, 3, 4, 5] -> [4, 5, 1, 2, 3]  k: 2
 
 
-    1st iteration:
+    1st reverse:
      0  1  2  3  4
      |  |  |  |  |
-    [1, 2, 3, 4, 5]
-    
+    [5, 4, 3, 2, 1]
 
+    2nd reverse: start: arr, last: arr + k - 1
+     s  l
+     |  |
+    [5, 4, 3, 2, 1]
+
+    [4, 5, 3, 2, 1]
+
+    3rd reverse: start: arr + k, last: arr + size -1
+           s     l
+           |     |
+    [4, 5, 3, 2, 1]
+
+    [4, 5, 1, 2, 3]
 */
